bde4aa41e3bd8963d9d90eb820a0ad4d2702a0af
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-31 10:40:53 -0300

    renderizando detalhes e criando retorno para dados que nao existem

--- Tutorial/polls/views.py
+++ Tutorial/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.template import loader
 
@@ -30,7 +30,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
