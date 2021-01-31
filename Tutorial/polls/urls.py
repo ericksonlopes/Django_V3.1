6581eff658b1379ae00f0de544bdb081ede4a128
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-31 11:14:36 -0300

    Namespacing nomes de URL, Escrevendo sua primeira aplicação Django, parte 3

--- Tutorial/polls/urls.py
+++ Tutorial/polls/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    # url e index da url
     path('', views.index, name='index'),
-
     path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.detail, name='results'),
-    path('<int:question_id>/vote/', views.detail, name='vote')
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- Tutorial/polls/views.py
+++ Tutorial/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 
 from .models import Question
@@ -29,11 +29,15 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
