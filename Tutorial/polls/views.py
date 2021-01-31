386dc3c13acbe90730f5105110a83b4b4e20d737
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-31 10:01:14 -0300

    Escreva views que façam algo, aprendendo sobre templates

--- Tutorial/polls/views.py
+++ Tutorial/polls/views.py
@@ -1,17 +1,26 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 # views
 # def index(request):
 #     # Retorna str, como response
 #     return HttpResponse("Hello, world. You're at the polls index.")
-from .models import Question
 
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
