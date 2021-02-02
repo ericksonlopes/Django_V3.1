3364cebfcfdccd3412d8059846a0230d85ef5230
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-02 09:17:34 -0300

    finalizando parte 4

--- Tutorial/polls/admin.py
+++ Tutorial/polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 # Registrando aplicação dentro do admin
 admin.site.register(Question)
+admin.site.register(Choice)
--- Tutorial/polls/views.py
+++ Tutorial/polls/views.py
@@ -44,7 +44,7 @@ def detail(request, question_id):
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls.html', {'question': question})
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
