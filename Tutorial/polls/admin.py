0812831b2159eba42ee4ebe25d5e6dc54f10f3ad
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-02 10:13:34 -0300

    end tutorial django

--- Tutorial/polls/admin.py
+++ Tutorial/polls/admin.py
@@ -13,10 +13,14 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
+        (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 admin.site.register(Question, QuestionAdmin)
