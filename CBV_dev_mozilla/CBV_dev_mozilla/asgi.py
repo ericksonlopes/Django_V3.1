d5d5f1b1c6767c737e21389345cb849d3a084685
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-02 14:52:31 -0300

    iniciando projeto cbv

---
+++ CBV_dev_mozilla/CBV_dev_mozilla/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CBV_dev_mozilla project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CBV_dev_mozilla.settings')
+
+application = get_asgi_application()
