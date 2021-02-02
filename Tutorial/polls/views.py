6002e124c861a06f000e3db7da999c14400f734c
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-02 08:55:09 -0300

    votos e resultados - Write a minimal form

--- Tutorial/polls/views.py
+++ Tutorial/polls/views.py
@@ -43,22 +43,21 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of questions. {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls.html', {'question': question})
 
 
 def vote(request, question_id):
-    question = get_object_or_404(Question, question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice.",
+            'error_message': "You didn't select a choice."
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+        return HttpResponseRedirect(reverse('polls:results'), args=(question_id, ))
