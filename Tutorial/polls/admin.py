f4fcd8c91be42e2fb2c2239507b17c7d7a40a36c
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-02 09:56:19 -0300

    Adicionando objetos relacionados

--- Tutorial/polls/admin.py
+++ Tutorial/polls/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
